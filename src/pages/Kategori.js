0e1608778a8511d95dfcc22e753ae3dacc3b87b7
fdhnuril3-source <fdhnuril3@gmail.com>
2025-10-29 10:38:58 +0700

    update Modul 2

---
+++ src/pages/Kategori.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Kategori(){
+    return (
+        <div>
+            <h1>Selamat datang di halaman Kategori </h1>
+        </div>
+    );
+}
+
+export default Kategori;
---
+++ src/pages/Produk.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Produk(){
+    return (
+        <div>
+            <h1>Selamat datang di halaman Produk </h1>
+        </div>
+    );
+}
+
+export default Produk;
